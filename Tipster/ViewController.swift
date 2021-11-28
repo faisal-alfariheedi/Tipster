0dac0fa09787841809e36d173f197a28550de02d
faisal alfariheedi <falfariheedi@gmail.com>
2021-11-28 17:08:06 +0300

    fin known bug some value doesn't get rounded although it used the same variable

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -19,9 +19,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var p10tip: UILabel!
     @IBOutlet weak var p10tot: UILabel!
     @IBOutlet weak var group: UILabel!
-    @IBOutlet weak var tp: UISlider!
-    @IBOutlet weak var gs: UISlider!
+    
+    var tp = Float(0)
+    var gs = Float(0)
     var tot:Double=0
+    var tott:String = "0"
     var con:Bool=true
     
     override func viewDidLoad() {
@@ -41,8 +43,10 @@ class ViewController: UIViewController {
     }
     @IBAction func ndot(_ sender: UIButton) {
         if(con){
-            ptot.text="\(String(describing: ptot.text))."
-            tot=Double(ptot.text!) ?? 0
+            tott="\(tott)."
+            ptot.text=tott
+            tot=Double(tott) ?? 0
+            con=false
             calc()
         }
     }
@@ -54,75 +58,114 @@ class ViewController: UIViewController {
     }
     @IBAction func n0(_ sender: UIButton) {
         if(tot != 0 || con != true ){
-        ptot.text="\(String(describing: ptot.text))0"
-        tot=Double(ptot.text!) ?? 0
+            tott="\(tott)0"
+            ptot.text=tott
+            tot=Double(tott) ?? 0
         calc()
         }
     }
     @IBAction func n1(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))1"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)1"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "1")}
+        ptot.text=tott
+        tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n2(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))2"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)2"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "2")}
+            
+            ptot.text=tott
+            tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n3(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))3"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)3"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "3")}
+        ptot.text=tott
+        tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n4(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))4"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)4"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "4")}
+        ptot.text=tott
+        tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n5(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))5"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)5"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "5")}
+        ptot.text=tott
+        tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n6(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))6"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)6"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "6")}
+        ptot.text=tott
+        tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n7(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))7"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)7"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "7")}
+        ptot.text=tott
+        tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n8(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))8"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)8"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "8")}
+            ptot.text=tott
+            tot=Double(tott) ?? 0
         calc()
     }
     @IBAction func n9(_ sender: UIButton) {
-        ptot.text="\(String(describing: ptot.text))9"
-        tot=Double(ptot.text!) ?? 0
+        if(tot != 0){
+        tott="\(tott)9"
+        }else {tott = tott.replacingOccurrences(of: "0", with: "9")}
+        ptot.text=tott
+        tot=Double(tott) ?? 0
         calc()
     }
-    @IBAction func tipslid(_ sender: UISlider) {
+    @IBAction func slidetip(_ sender: UISlider) {
+        tp = round(sender.value*100)/100
         calc()
-        
     }
-    @IBAction func groupslid(_ sender: UISlider) {
-        group.text="Group Size:\(Int(gs.value))"
+    @IBAction func slidegroup(_ sender: UISlider) {
+        group.text="Group Size:\(Int(sender.value))"
+        gs = round(sender.value)
+        calc()
     }
+   
     
     
     func calc(){
-        p0.text="\(String((round(tp.value*100)/100)*100))%"
-        p5.text="\(String(((round(tp.value*100)/100)+5)*100))%"
-        p10.text="\(String(((round(tp.value*100)/100)+10)*100))%"
-        p0tip.text=String(tot*Double(p0.text/100))
-        p5tip.text=String(tot*Double(p5.text/100))
-        p10tip.text=String(tot*Double(p10.text/100))
-        p0tot.text=String(round(gs.value)*Double(p0tip.text)+tot)
-        p5tot.text=String(round(gs.value)*Double(p5tip.text)+tot)
-        p10tot.text=String(round(gs.value)*Double(p10tip.text)+tot)
+        var grp=Double(round(gs))
+        var tip=(round(tp*100)/100)
+        p0.text="\(tp)%"
+        p5.text = "\(tp+5)%"
+        p10.text = "\(tp+10)%"
+        var pp0 : Double = Double(p0.text!.replacingOccurrences(of: "%", with: ""))!
+        var pp5 : Double = Double(p5.text!.replacingOccurrences(of: "%", with: ""))!
+        var pp10 : Double = Double(p10.text!.replacingOccurrences(of: "%", with: ""))!
+        
+        
+        p0tip.text = String(tot * pp0/100)
+        p5tip.text = String(tot * pp5/100)
+        p10tip.text = String(tot * pp10/100)
+        p0tot.text = String(Double(gs) * Double(p0tip.text!)!+tot)
+        p5tot.text = String(Double(gs) * Double(p5tip.text!)!+tot)
+        p10tot.text = String(Double(gs) * Double(p10tip.text!)!+tot)
     }
     
 }
